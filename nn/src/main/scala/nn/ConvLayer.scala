86433e1c044d67ec3cf080448bf3e39db4407de1
Marcel Luethi <marcel.luethi@inf.ethz.ch>
2026-01-10 12:17:32 +0100

    add conv layer

---
+++ nn/src/main/scala/nn/ConvLayer.scala
@@ -0,0 +1,58 @@
+package nn
+
+import dimwit.*
+import dimwit.random.Random
+import dimwit.random.Random.Key
+import dimwit.tensor.{VType, ExecutionType, Labels, Tensor, Shape, Axis, Dim}
+import dimwit.tensor.TensorOps.Convolution.Padding
+import dimwit.tensor.TupleHelpers.{Last, ReplaceLast}
+import dimwit.stats.Normal
+
+object ConvLayer:
+
+  case class Params[InChannels, OutChannels, KernelShape <: Tuple](
+      kernel: Tensor[KernelShape, Float],
+      bias: Tensor1[OutChannels, Float]
+  )
+
+  object Params:
+    given [IC: Label, OC: Label, KS <: Tuple: Labels]: TensorTree[Params[IC, OC, KS]] = TensorTree.derived
+    given [IC: Label, OC: Label, KS <: Tuple: Labels]: ToPyTree[Params[IC, OC, KS]] = ToPyTree.derived
+
+    /** Initialize convolutional layer parameters
+      *
+      * @param paramKey
+      *   Random key for parameter initialization
+      * @param kernelShape
+      *   Shape of the convolutional kernel, e.g., (KernelH, KernelW, InChannels, OutChannels) for 2D conv
+      * @param outChannelDim
+      *   Dimension specification for output channels
+      */
+    def apply[InChannels: Label, OutChannels: Label, KernelShape <: Tuple: Labels](paramKey: Key)(
+        kernelShape: Shape[KernelShape],
+        outChannelDim: Dim[OutChannels]
+    )(using
+        executionType: ExecutionType[Float]
+    ): Params[InChannels, OutChannels, KernelShape] =
+      Params(
+        kernel = Normal.standardNormal(kernelShape).sample(paramKey),
+        bias = Tensor.zeros(Shape(outChannelDim), VType[Float])
+      )
+
+case class ConvLayer[InChannels: Label, OutChannels: Label, KernelShape <: Tuple: Labels](
+    params: ConvLayer.Params[InChannels, OutChannels, KernelShape],
+    stride: Int = 1,
+    padding: Padding = Padding.SAME
+):
+  /** Apply convolution to input tensor
+    *
+    * Input should have shape (Batch, Spatial..., InChannels) Output will have shape (Batch, Spatial..., OutChannels)
+    */
+  def apply[InputShape <: Tuple: Labels](x: Tensor[InputShape, Float])(using
+      inputChannelMatch: Last[InputShape] =:= InChannels,
+      kernelInMatch: dimwit.tensor.TupleHelpers.SecondToLast[KernelShape] =:= InChannels,
+      kernelOutMatch: Last[KernelShape] =:= OutChannels,
+      outputLabels: Labels[ReplaceLast[InputShape, OutChannels]]
+  ): Tensor[ReplaceLast[InputShape, OutChannels], Float] =
+    import params.{kernel, bias}
+    x.conv(Axis[InChannels], Axis[OutChannels])(kernel, stride, padding) +! bias
